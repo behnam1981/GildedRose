7a8723d2efdfea042a732ced7d4119fdc2172180
bh-bor <bh@blue-ocean-robotics.com>
2018-08-09 09:21:42 +0200

    Add service repo to the project

---
+++ src/GildedRose.Console/Services/Service.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GildedRose.Console.Services
+{
+    /// <summary>
+    /// Services Repository with Per-Request Instances
+    /// </summary>
+    public class Service
+    {
+        // ----------------------
+        // Exported Services
+        // ----------------------
+
+        public static ItemService ItemService => GetInstance(() => { return new ItemService(Program.Items); });
+
+
+        /// <summary>
+        /// Get Service Instance
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="Factory"></param>
+        /// <returns></returns>
+        private static T GetInstance<T>(Func<T> Factory)
+        {
+            return Factory.Invoke();
+        }
+
+
+    }
+}
